75f9bfbfdf3c6cf51ab82aedacc1a7816064b2ed
chg95211 <chg95211@163.com>
2018-07-11 16:02:15 +0800

    demo

--- OpencvsharpDemo/Form1.cs
+++ OpencvsharpDemo/Form1.cs
@@ -21,6 +21,14 @@ namespace OpencvsharpDemo
         private void Form1_Load(object sender, EventArgs e)
         {
             Mat src = new Mat("lenna.png", ImreadModes.GrayScale);
+            Mat dst = new Mat();
+
+            Cv2.Canny(src, dst, 50, 200);
+            using (new Window("src image", src))
+            using (new Window("dst image", dst))
+            {
+                Cv2.WaitKey();
+            }
         }
     }
 }
